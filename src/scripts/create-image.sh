91b8ad952dcb1c4231104e333596e9bbe3ed19a4
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2023-06-10 13:11:15 +0100

    script now creates image for lbas and workload/webserver

--- src/scripts/create-image.sh
+++ src/scripts/create-image.sh
@@ -1,23 +1,43 @@
 #!/usr/bin/env bash
 
+# check number of arguments. if different that 1, abort
+if [ "$#" -ne 1 ]; then
+	echo "Usage: $0 [webserver|lbas]"
+	exit 1
+fi
+
+# check if argument is valid
+if [ "$1" != "webserver" ] && [ "$1" != "lbas" ]; then
+	echo "Usage: $0 [webserver|lbas]"
+	exit 1
+fi
+
 source config.sh
 
 # Step 1: launch a vm instance.
 $DIR/launch-vm.sh
 
 # Step 2: install software in the VM instance.
-$DIR/install-vm.sh
+$DIR/install-vm.sh "$1"
 
 # Step 3: test VM instance.
 $DIR/test-vm.sh
 
 # Step 4: create VM image (AIM).
-aws ec2 create-image --instance-id $(cat instance.id) --name CNV-Image | jq -r .ImageId > image.id
+if [ "$1" = "webserver" ]; then
+	aws ec2 create-image --instance-id $(cat instance.id) --name CNV-Webserver | jq -r .ImageId > image.id
+else
+	aws ec2 create-image --instance-id $(cat instance.id) --name CNV-LBAS | jq -r .ImageId > image.id
+fi
 echo "New VM image with id $(cat image.id)."
 
 # Step 5: Wait for image to become available.
 echo "Waiting for image to be ready... (this can take a couple of minutes)"
-aws ec2 wait image-available --filters Name=name,Values=CNV-Image
+if [ "$1" = "webserver" ]; then
+	aws ec2 wait image-available --filters Name=name,Values=CNV-Webserver
+else
+	aws ec2 wait image-available --filters Name=name,Values=CNV-LBAS
+fi
 echo "Waiting for image to be ready... done! \o/"
 
 # Step 6: terminate the vm instance.
--- src/scripts/install-vm.sh
+++ src/scripts/install-vm.sh
@@ -1,16 +1,39 @@
 #!/usr/bin/env bash
 
+# check number of arguments. if different that 1, abort
+if [ "$#" -ne 1 ]; then
+	echo "Usage: $0 [webserver|lbas]"
+	exit 1
+fi
+
+# check if argument is valid
+if [ "$1" != "webserver" ] && [ "$1" != "lbas" ]; then
+	echo "Usage: $0 [webserver|lbas]"
+	exit 1
+fi
+
 source config.sh
 
 # Install java.
 cmd="sudo yum update -y && sudo yum install java-11-amazon-corretto.x86_64 -y"
 ssh -o StrictHostKeyChecking=no -i $AWS_EC2_SSH_KEYPAIR_PATH ec2-user@$(cat instance.dns) $cmd
 
+# Send config file to instance.
+scp -o StrictHostKeyChecking=no -i $AWS_EC2_SSH_KEYPAIR_PATH $DIR/../scripts/config.sh ec2-user@$(cat instance.dns):
+
 # Send jars to instance.
-scp -o StrictHostKeyChecking=no -i $AWS_EC2_SSH_KEYPAIR_PATH $DIR/../javassist/target/JavassistWrapper-1.0-jar-with-dependencies.jar ec2-user@$(cat instance.dns):
-scp -o StrictHostKeyChecking=no -i $AWS_EC2_SSH_KEYPAIR_PATH $DIR/../workload/webserver/target/webserver-1.0.0-SNAPSHOT-jar-with-dependencies.jar ec2-user@$(cat instance.dns):
+if [ "$1" = "webserver" ]; then
+	scp -o StrictHostKeyChecking=no -i $AWS_EC2_SSH_KEYPAIR_PATH $DIR/../javassist/target/JavassistWrapper-1.0-jar-with-dependencies.jar ec2-user@$(cat instance.dns):
+	scp -o StrictHostKeyChecking=no -i $AWS_EC2_SSH_KEYPAIR_PATH $DIR/../workload/webserver/target/webserver-1.0.0-SNAPSHOT-jar-with-dependencies.jar ec2-user@$(cat instance.dns):
+else
+	scp -o StrictHostKeyChecking=no -i $AWS_EC2_SSH_KEYPAIR_PATH $DIR/../lbas/target/LBAS-1.0-jar-with-dependencies.jar ec2-user@$(cat instance.dns):
+fi
 
 # Setup the webserver to start at boot.
-java_cmd="cd /home/ec2-user && java -cp webserver-1.0.0-SNAPSHOT-jar-with-dependencies.jar -Xbootclasspath/a:webserver-1.0.0-SNAPSHOT-jar-with-dependencies.jar,JavassistWrapper-1.0-jar-with-dependencies.jar -javaagent:JavassistWrapper-1.0-jar-with-dependencies.jar=ICount:pt.ulisboa.tecnico.cnv,javax.imageio:output pt.ulisboa.tecnico.cnv.webserver.WebServer"
+if [ "$1" = "webserver" ]; then
+	java_cmd="cd /home/ec2-user && source config.sh && java -cp webserver-1.0.0-SNAPSHOT-jar-with-dependencies.jar -Xbootclasspath/a:webserver-1.0.0-SNAPSHOT-jar-with-dependencies.jar,JavassistWrapper-1.0-jar-with-dependencies.jar -javaagent:JavassistWrapper-1.0-jar-with-dependencies.jar=ICount:pt.ulisboa.tecnico.cnv,javax.imageio:output pt.ulisboa.tecnico.cnv.webserver.WebServer"
+else
+	java_cmd="cd /home/ec2-user && source config.sh && java -cp LBAS-1.0-jar-with-dependencies.jar pt.ulisboa.tecnico.cnv.lbas.LBAS"
+fi
 cmd="echo \"$java_cmd\" | sudo tee -a /etc/rc.local && sudo chmod +x /etc/rc.local"
 ssh -o StrictHostKeyChecking=no -i $AWS_EC2_SSH_KEYPAIR_PATH ec2-user@$(cat instance.dns) $cmd
