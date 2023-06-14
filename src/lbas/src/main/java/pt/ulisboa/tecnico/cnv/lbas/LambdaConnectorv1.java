8b2df8fcccbeb7f8a3fbc08f998d8b0444955609
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2023-06-14 11:53:07 +0100

    added v1 connector

---
+++ src/lbas/src/main/java/pt/ulisboa/tecnico/cnv/lbas/LambdaConnectorv1.java
@@ -0,0 +1,53 @@
+package pt.ulisboa.tecnico.cnv.lbas;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.lambda.AWSLambda;
+import com.amazonaws.services.lambda.AWSLambdaClientBuilder;
+import com.amazonaws.services.lambda.model.InvokeRequest;
+import com.amazonaws.services.lambda.model.InvokeResult;
+
+public class LambdaConnectorv1 {
+
+  public static String payloadGenerator(Map<String, String> parameters) {
+    String ret = "{";
+
+    for (Map.Entry<String, String> entry : parameters.entrySet()) {
+      ret += "\"" + entry.getKey() + "\":\"" + entry.getValue() + "\",";
+    }
+    ret = ret.substring(0, ret.length() - 1) + "}";
+
+    System.out.println(ret);
+    return ret;
+  }
+
+  public static String invokeFunction(AWSLambda awsLambda, String functionName, String json) {
+    String value = null;
+    try {
+      InvokeRequest request = new InvokeRequest()
+        .withFunctionName(functionName)
+        .withPayload(json);
+
+      InvokeResult res = awsLambda.invoke(request);
+      value = new String(res.getPayload().array(), StandardCharsets.UTF_8);
+      System.out.println(value);
+
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+
+    return value;
+  }
+
+  public static void main(String[] args) {
+    AWSLambda awsLambda = AWSLambdaClientBuilder.standard()
+      .withCredentials(new EnvironmentVariableCredentialsProvider())
+      .withRegion(Regions.US_EAST_1)
+      .build();
+
+    invokeFunction(awsLambda, "CNV-InsectWars", "{\"max\":\"2\",\"army1\":\"10\",\"army2\":\"9\"}");
+  }
+}
